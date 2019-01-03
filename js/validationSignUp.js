a6523e051b8e1cb8eae92aa3dfb9498c7872384e
omri eitan <omrieitan@icloud.com>
2019-01-03 15:21:37 +0200

    refactor js

--- js/validationSignUp.js
+++ js/validationSignUp.js
@@ -8,68 +8,61 @@ function validation() {
     const date = document.signup.birthday.value;
     const height = document.signup.height.value;
     const weight = document.signup.weight.value;
-    var flag=true;
+    let flag = true;
     if (username === '') {
         document.getElementById('usernameValid').innerHTML = 'user name is empty, please put a username ';
-        flag= false;
+        flag = false;
     }
-    if (document.signup.checkbox.checked===false) {
+    if (isUserExistOnSubmit(username))
+        flag = false;
+    if (document.signup.checkbox.checked === false) {
         alert('you must agree to our terms');
-        flag= false;
+        flag = false;
     }
     if (firstname === '') {
         document.getElementById('firstnameValid').innerHTML = 'first name is empty, please put a first name ';
-        flag= false;
-
+        flag = false;
     }
     if (lastname === '') {
         document.getElementById('lastnameValid').innerHTML = 'last name is empty, please put a last name ';
-        flag= false;
-
+        flag = false;
     }
     if (email === '') {
         document.getElementById('emailValid').innerHTML = 'email is empty, please put an email ';
-        flag= false;
-
+        flag = false;
     }
     if (password === '') {
         document.getElementById('passwordValid').innerHTML = 'password is empty, please put a password ';
-        flag= false;
-
-    }else {
-        if (password.length<=5){
+        flag = false;
+    } else {
+        if (password.length <= 5) {
             document.getElementById('passwordValid').innerHTML = 'password is short, please choose a longer password ';
-            flag= false;
-
+            flag = false;
         }
     }
     if (confirmpassword === '') {
         document.getElementById('confirmpasswordValid').innerHTML = 'confirm password is empty, please put a confirm password ';
-        flag= false;
+        flag = false;
 
-    }else {
+    } else {
         if (confirmpassword.localeCompare(password)) {
             document.getElementById('confirmpasswordValid').innerHTML = 'confirm password is not the same, please put a correct confirm password ';
-            flag= false;
-
+            flag = false;
         }
     }
     if (date === '') {
         document.getElementById('birthdayValid').innerHTML = 'birthday is empty, please put a birthday';
-        flag= false;
-
+        flag = false;
     }
     if (height === '') {
         document.getElementById('heightValid').innerHTML = 'height is empty, please put a height';
-        flag= false;
-
+        flag = false;
     }
     if (weight === '') {
         document.getElementById('weightValid').innerHTML = 'weight is empty, please put a height';
-        flag= false;
-
+        flag = false;
     }
-return flag;
+    return flag;
 }
 
 let request;
@@ -80,11 +73,10 @@ if (navigator.appName === 'Microsoft Internet Explorer') {
 }
 
 function isUserExist(username) {
-
     request.abort();
     request.open('GET', 'http://localhost:8080/controller/UserController/isExistUserName?userName=' + username, true);
     request.onreadystatechange = function () {
-        if (request.readyState === 4 && request.status===200) {
+        if (request.readyState === 4 && request.status === 200) {
             let str = '';
             if (request.responseText === 'true') //the username already exist
                 str = 'the username already exist';
@@ -92,6 +84,18 @@ function isUserExist(username) {
         }
     };
     request.send(null);
+}
 
-
+function isUserExistOnSubmit(username) {
+    request.abort();
+    request.open('GET', 'http://localhost:8080/controller/UserController/isExistUserName?userName=' + username, true);
+    let flag = false;
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            if (request.responseText === 'true') //the username already exist
+                flag = true;
+        }
+    };
+    request.send(null);
+    return flag;
 }
